8b278ff3e3d2814d46c294b2dac00551cfaf39c4
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-20 21:52:07 +0100

    add MotofleaMarket

---
+++ src/test/java/EnumStorage/MotofleaMarket.java
@@ -0,0 +1,27 @@
+package EnumStorage;
+
+public enum MotofleaMarket {
+    MOTOFLEA_MARKET("Мотобарахолка"),
+    APRILIA("Aprilia"),
+    BMW("BMW"),
+    HARLEY_DAVIDSON("Harley-Davidson"),
+    HONDA("Honda"),
+    HORS("HORS"),
+    JAWA("Jawa"),
+    KAWASAKI("Kawasaki"),
+    SUZUKI("Suzuki"),
+    VIPER("Viper"),
+    YAMAHA("Yamaha"),
+    DNEPR("Днепр"),
+    MINSK("Минск");
+
+    String nameMotofleaMarket;
+
+    MotofleaMarket(String nameMotofleaMarket) {
+        this.nameMotofleaMarket = nameMotofleaMarket;
+    }
+
+    public String getName() {
+        return nameMotofleaMarket;
+    }
+}
