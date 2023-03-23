66ba9d3db84fc7d09fbec6f73f8a22d7fb76b6ec
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-23 20:39:03 +0100

    add src/test/java/enumStorage/HousesAndApartmentsRent.java

---
+++ src/test/java/enumStorage/HousesAndApartmentsRent.java
@@ -0,0 +1,29 @@
+package enumStorage;
+
+public enum HousesAndApartmentsRent {
+    MINSK("Минск"),
+    BREST("Брест"),
+    VITEBSK("Витебск"),
+    HOMEL("Гомель"),
+    HRODNO("Гродно"),
+    MAHILEV("Могилев"),
+    ONE_ROOM("1-комнатные"),
+    TWO_ROOM("2-комнатные"),
+    THREE_ROOM("3-комнатные"),
+    FOUR_PLUS_ROOM("4+-комнатные"),
+    ROOMS("Комнаты"),
+    FROM_OWNER("От собственника"),
+    UP_TO_250("До 250 $"),
+    FROM_250_TO_500("250-500 $"),
+    FROM_500("От 500 $");
+
+    String nameHousesAndApartmentsRent;
+
+    HousesAndApartmentsRent(String nameHousesAndApartmentsRent) {
+        this.nameHousesAndApartmentsRent = nameHousesAndApartmentsRent;
+    }
+
+    public String getName() {
+        return nameHousesAndApartmentsRent;
+    }
+}
